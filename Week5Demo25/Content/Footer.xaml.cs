4fd0b48ac99b5d51677b611ffc226be4fef4ae81
Shaun Gregory Stummer <141109489+TAFESHAUN@users.noreply.github.com>
2025-03-05 11:51:00 +1100

    UpdatedWithContent

---
+++ Week5Demo25/Content/Footer.xaml.cs
@@ -0,0 +1,9 @@
+namespace Week5Demo25.Content;
+
+public partial class Footer : ContentView
+{
+	public Footer()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Week5Demo25/Content/Header.xaml.cs
@@ -0,0 +1,9 @@
+namespace Week5Demo25.Content;
+
+public partial class Header : ContentView
+{
+	public Header()
+	{
+		InitializeComponent();
+	}
+}
